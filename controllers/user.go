fe6e3207b608f2e897547362a193f1a136572f6e
aleatory_freak <iakhilmeena@gmail.com>
2024-02-12 20:57:59 +0530

    bug fixes for virtual save & send cert

--- controllers/user.go
+++ controllers/user.go
@@ -81,42 +81,84 @@ func SendHeart_Stage1(c *gin.Context) {
 	}
 
 	userID, _ := c.Get("user_id")
+
+	// Use a separate database session for each operation
+	// to avoid conflicts with table name "users" specified more than once
+	tx := Db.Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+
 	var user models.User
-	record := Db.Model(&user).Where("id = ?", userID).First(&user)
+	record := tx.Model(&user).Where("id = ?", userID).First(&user)
 	if record.Error != nil {
+		tx.Rollback()
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Something went wrong, Please try again."})
 		return
 	}
 	if user.S1submit {
+		tx.Rollback()
 		c.JSON(http.StatusUnauthorized, gin.H{"error": "Hearts already sent."})
 		return
 	}
 
-	newHeart := models.Stage1{
-		Id: userID.(string),
-		M1: info.M1,
-		M2: info.M2,
-		M3: info.M3,
-		M4: info.M4,
+	// Check if the entry with userID exists
+	existingHeart := models.Stage1{}
+	if err := tx.Where("id = ?", userID.(string)).First(&existingHeart).Error; err != nil {
+		// Entry not found, create a new one
+		if gorm.ErrRecordNotFound == err { // Should've written opposite ?  -- this one has better readability ;}
+			newHeart := models.Stage1{
+				Id:     userID.(string),
+				M1:     info.M1,
+				M2:     info.M2,
+				M3:     info.M3,
+				M4:     info.M4,
+				Submit: true,
+			}
+
+			if err := tx.Create(&newHeart).Error; err != nil {
+				tx.Rollback()
+				c.JSON(http.StatusBadRequest, gin.H{"error": err})
+				return
+			}
+		} else {
+			// Other error occurred
+			tx.Rollback()
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Something went wrong, Please try again."})
+			return
+		}
+	} else {
+		// Entry found, update the existing one
+		existingHeart.M1 = info.M1
+		existingHeart.M2 = info.M2
+		existingHeart.M3 = info.M3
+		existingHeart.M4 = info.M4
+		existingHeart.Submit = true
+		if err := tx.Save(&existingHeart).Error; err != nil {
+			tx.Rollback()
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Something went wrong, Please try again."})
+			return
+		}
 	}
 
-	if err := Db.Create(&newHeart).Error; err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err})
-		return
-	}
-
-	if err := record.Updates(models.User{
+	if err := tx.Model(&user).Where("id = ?", userID).Updates(models.User{
+		S1Data:   info.S1Data,
 		S1submit: true,
-		S1Data: info.S1Data,
 	}).Error; err != nil {
+		tx.Rollback()
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Something went wrong, Please try again."})
 		return
 	}
 
+	// Commit the transaction if everything is successful
+	tx.Commit()
+
 	c.JSON(http.StatusAccepted, gin.H{"message": "Hearts Sent Successfully !!"})
 }
 
-// TODO: Will implement virtualization later ;)
+// TODO: Will implement virtualization later ;) -- Done
 // func Stage1_Virtual(c *gin.Context) {
 func SendHeartVirtual_Stage1(c *gin.Context) {
 	// User already authenticated in router.go by gin.HandlerFunc
@@ -128,36 +170,78 @@ func SendHeartVirtual_Stage1(c *gin.Context) {
 	}
 
 	userID, _ := c.Get("user_id")
+
+	// Use a separate database session for each operation
+	// to avoid conflicts with table name "users" specified more than once
+	tx := Db.Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+
 	var user models.User
-	record := Db.Model(&user).Where("id = ?", userID).First(&user)
+	record := tx.Model(&user).Where("id = ?", userID).First(&user)
 	if record.Error != nil {
+		tx.Rollback()
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Something went wrong, Please try again."})
 		return
 	}
 	if user.S1submit {
+		tx.Rollback()
 		c.JSON(http.StatusUnauthorized, gin.H{"error": "Hearts already sent."})
 		return
 	}
 
-	newHeart := models.Stage1{
-		Id: userID.(string),
-		M1: info.M1,
-		M2: info.M2,
-		M3: info.M3,
-		M4: info.M4,
+	// Check if the entry with userID exists
+	existingHeart := models.Stage1{}
+	if err := tx.Where("id = ?", userID.(string)).First(&existingHeart).Error; err != nil {
+		// Entry not found, create a new one
+		if gorm.ErrRecordNotFound == err { // Should've written opposite ?  -- this one has better readability ;}
+			newHeart := models.Stage1{
+				Id:     userID.(string),
+				M1:     info.M1,
+				M2:     info.M2,
+				M3:     info.M3,
+				M4:     info.M4,
+				Submit: false,
+			}
+
+			if err := tx.Create(&newHeart).Error; err != nil {
+				tx.Rollback()
+				c.JSON(http.StatusBadRequest, gin.H{"error": err})
+				return
+			}
+		} else {
+			// Other error occurred
+			tx.Rollback()
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Something went wrong, Please try again."})
+			return
+		}
+	} else {
+		// Entry found, update the existing one
+		existingHeart.M1 = info.M1
+		existingHeart.M2 = info.M2
+		existingHeart.M3 = info.M3
+		existingHeart.M4 = info.M4
+
+		if err := tx.Save(&existingHeart).Error; err != nil {
+			tx.Rollback()
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Something went wrong, Please try again."})
+			return
+		}
 	}
 
-	if err := Db.Create(&newHeart).Error; err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err})
-		return
-	}
-
-	if err := record.Updates(models.User{
+	if err := tx.Model(&user).Where("id = ?", userID).Updates(models.User{
 		S1Data: info.S1Data,
 	}).Error; err != nil {
+		tx.Rollback()
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Something went wrong, Please try again."})
 		return
 	}
+
+	// Commit the transaction if everything is successful
+	tx.Commit()
 
 	c.JSON(http.StatusAccepted, gin.H{"message": "Hearts Sent Successfully !!"})
 }
@@ -172,53 +256,113 @@ func SendHeart_Stage2(c *gin.Context) {
 	}
 
 	userID, _ := c.Get("user_id")
+
+	// Use a separate database session for each operation
+	// to avoid conflicts with table name "users" specified more than once
+	tx := Db.Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+
 	var user models.User
-	record := Db.Model(&user).Where("id = ?", userID).First(&user)
+	record := tx.Model(&user).Where("id = ?", userID).First(&user)
 	if record.Error != nil {
+		tx.Rollback()
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Something went wrong, Please try again."})
 		return
 	}
 	if user.S2submit {
+		tx.Rollback()
 		c.JSON(http.StatusUnauthorized, gin.H{"error": "Hearts already sent."})
 		return
 	}
 
-	newHeart := models.Stage2{
-		Id : userID.(string),
-		Ka1 : info.Ka1,
-		Ka2 : info.Ka2,
-		Ka3 : info.Ka3,
-		Ka4 : info.Ka4,
-		Kb1 : info.Kb1,
-		Kb2 : info.Kb2,
-		Kb3 : info.Kb3,
-		Kb4 : info.Kb4,
-		Kc1 : info.Kc1,
-		Kc2 : info.Kc2,
-		Kc3 : info.Kc3,
-		Kc4 : info.Kc4,
-		Kd1 : info.Kd1,
-		Kd2 : info.Kd2,
-		Kd3 : info.Kd3,
-		Kd4 : info.Kd4,
-		CertA : info.CertA,
-		CertB : info.CertB,
-		CertC : info.CertC,
-		CertD : info.CertD,
+	// Check if the entry with userID exists
+	existingHeart := models.Stage2{}
+	if err := tx.Where("id = ?", userID.(string)).First(&existingHeart).Error; err != nil {
+		// Entry not found, create a new one
+		if gorm.ErrRecordNotFound == err { // Should've written opposite ?  -- this one has better readability ;}
+			newHeart := models.Stage2{
+				Id:     userID.(string),
+				Ka1:    info.Ka1,
+				Ka2:    info.Ka2,
+				Ka3:    info.Ka3,
+				Ka4:    info.Ka4,
+				Kb1:    info.Kb1,
+				Kb2:    info.Kb2,
+				Kb3:    info.Kb3,
+				Kb4:    info.Kb4,
+				Kc1:    info.Kc1,
+				Kc2:    info.Kc2,
+				Kc3:    info.Kc3,
+				Kc4:    info.Kc4,
+				Kd1:    info.Kd1,
+				Kd2:    info.Kd2,
+				Kd3:    info.Kd3,
+				Kd4:    info.Kd4,
+				CertA:  info.CertA,
+				CertB:  info.CertB,
+				CertC:  info.CertC,
+				CertD:  info.CertD,
+				Submit: true,
+			}
+
+			if err := tx.Create(&newHeart).Error; err != nil {
+				tx.Rollback()
+				c.JSON(http.StatusBadRequest, gin.H{"error": err})
+				return
+			}
+		} else {
+			// Other error occurred - maybe due to db
+			tx.Rollback()
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Something went wrong, Please try again."})
+			return
+		}
+	} else {
+		// Entry found, update the existing one
+		existingHeart.Ka1 = info.Ka1
+		existingHeart.Ka2 = info.Ka2
+		existingHeart.Ka3 = info.Ka3
+		existingHeart.Ka4 = info.Ka4
+		existingHeart.Kb1 = info.Kb1
+		existingHeart.Kb2 = info.Kb2
+		existingHeart.Kb3 = info.Kb3
+		existingHeart.Kb4 = info.Kb4
+		existingHeart.Kc1 = info.Kc1
+		existingHeart.Kc2 = info.Kc2
+		existingHeart.Kc3 = info.Kc3
+		existingHeart.Kc4 = info.Kc4
+		existingHeart.Kd1 = info.Kd1
+		existingHeart.Kd2 = info.Kd2
+		existingHeart.Kd3 = info.Kd3
+		existingHeart.Kd4 = info.Kd4
+		existingHeart.CertA = info.CertA
+		existingHeart.CertB = info.CertB
+		existingHeart.CertC = info.CertC
+		existingHeart.CertD = info.CertD
+		existingHeart.Submit = true
+
+		if err := tx.Save(&existingHeart).Error; err != nil {
+			tx.Rollback()
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Something went wrong, Please try again."})
+			return
+		}
 	}
 
-	if err := Db.Create(&newHeart).Error; err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err})
-		return
-	}
-
-	if err := record.Updates(models.User{
+	if err := tx.Model(&user).Where("id = ?", userID).Updates(models.User{
 		S2submit: true,
-		S2Data: info.S2Data,
+		S2Data:   info.S2Data,
 	}).Error; err != nil {
+		tx.Rollback()
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Something went wrong, Please try again."})
 		return
 	}
+
+	tx.Commit()
+
+	c.JSON(http.StatusAccepted, gin.H{"message": "Hearts Sent Successfully !!"})
 }
 
 func SendHeartVirtual_Stage2(c *gin.Context) {
@@ -231,67 +375,172 @@ func SendHeartVirtual_Stage2(c *gin.Context) {
 	}
 
 	userID, _ := c.Get("user_id")
+
+	// Use a separate database session for each operation
+	// to avoid conflicts with table name "users" specified more than once
+	tx := Db.Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+
+	var user models.User
+	record := tx.Model(&user).Where("id = ?", userID).First(&user)
+	if record.Error != nil {
+		tx.Rollback()
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Something went wrong, Please try again."})
+		return
+	}
+	if user.S2submit {
+		tx.Rollback()
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Hearts already sent."})
+		return
+	}
+
+	// Check if the entry with userID exists
+	existingHeart := models.Stage2{}
+	if err := tx.Where("id = ?", userID.(string)).First(&existingHeart).Error; err != nil {
+		// Entry not found, create a new one
+		if gorm.ErrRecordNotFound == err { // Should've written opposite ?  -- this one has better readability ;}
+			newHeart := models.Stage2{
+				Id:     userID.(string),
+				Ka1:    info.Ka1,
+				Ka2:    info.Ka2,
+				Ka3:    info.Ka3,
+				Ka4:    info.Ka4,
+				Kb1:    info.Kb1,
+				Kb2:    info.Kb2,
+				Kb3:    info.Kb3,
+				Kb4:    info.Kb4,
+				Kc1:    info.Kc1,
+				Kc2:    info.Kc2,
+				Kc3:    info.Kc3,
+				Kc4:    info.Kc4,
+				Kd1:    info.Kd1,
+				Kd2:    info.Kd2,
+				Kd3:    info.Kd3,
+				Kd4:    info.Kd4,
+				CertA:  info.CertA,
+				CertB:  info.CertB,
+				CertC:  info.CertC,
+				CertD:  info.CertD,
+				Submit: false,
+			}
+
+			if err := tx.Create(&newHeart).Error; err != nil {
+				tx.Rollback()
+				c.JSON(http.StatusBadRequest, gin.H{"error": err})
+				return
+			}
+		} else {
+			// Other error occurred - maybe due to db
+			tx.Rollback()
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Something went wrong, Please try again."})
+			return
+		}
+	} else {
+		// Entry found, update the existing one
+		existingHeart.Ka1 = info.Ka1
+		existingHeart.Ka2 = info.Ka2
+		existingHeart.Ka3 = info.Ka3
+		existingHeart.Ka4 = info.Ka4
+		existingHeart.Kb1 = info.Kb1
+		existingHeart.Kb2 = info.Kb2
+		existingHeart.Kb3 = info.Kb3
+		existingHeart.Kb4 = info.Kb4
+		existingHeart.Kc1 = info.Kc1
+		existingHeart.Kc2 = info.Kc2
+		existingHeart.Kc3 = info.Kc3
+		existingHeart.Kc4 = info.Kc4
+		existingHeart.Kd1 = info.Kd1
+		existingHeart.Kd2 = info.Kd2
+		existingHeart.Kd3 = info.Kd3
+		existingHeart.Kd4 = info.Kd4
+		existingHeart.CertA = info.CertA
+		existingHeart.CertB = info.CertB
+		existingHeart.CertC = info.CertC
+		existingHeart.CertD = info.CertD
+
+		if err := tx.Save(&existingHeart).Error; err != nil {
+			tx.Rollback()
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Something went wrong, Please try again."})
+			return
+		}
+	}
+
+	if err := tx.Model(&user).Where("id = ?", userID).Updates(models.User{
+		S2Data: info.S2Data,
+	}).Error; err != nil {
+		tx.Rollback()
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Something went wrong, Please try again."})
+		return
+	}
+
+	tx.Commit()
+
+	c.JSON(http.StatusAccepted, gin.H{"message": "Hearts Sent Successfully !!"})
+}
+
+
+func Send_Cert(c *gin.Context){
+	// User already authenticated in router.go by gin.HandlerFunc
+	// TODO: ADD a permit variable to control the api call output ie. accept or reject
+	info := new(models.Share_Cert)
+	if err := c.BindJSON(info); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Input data format."})
+		return
+	}
+
+	userID, _ := c.Get("user_id")
 	var user models.User
 	record := Db.Model(&user).Where("id = ?", userID).First(&user)
 	if record.Error != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Something went wrong, Please try again."})
 		return
 	}
-	if user.S2submit {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Hearts already sent."})
+	if user.Certgiven {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Certificates already sent."})
 		return
 	}
 
-	newHeart := models.Stage2{
-		Id : userID.(string),
-		Ka1 : info.Ka1,
-		Ka2 : info.Ka2,
-		Ka3 : info.Ka3,
-		Ka4 : info.Ka4,
-		Kb1 : info.Kb1,
-		Kb2 : info.Kb2,
-		Kb3 : info.Kb3,
-		Kb4 : info.Kb4,
-		Kc1 : info.Kc1,
-		Kc2 : info.Kc2,
-		Kc3 : info.Kc3,
-		Kc4 : info.Kc4,
-		Kd1 : info.Kd1,
-		Kd2 : info.Kd2,
-		Kd3 : info.Kd3,
-		Kd4 : info.Kd4,
-		CertA : info.CertA,
-		CertB : info.CertB,
-		CertC : info.CertC,
-		CertD : info.CertD,
+	newCert := models.Stage3{
+		Id: userID.(string),
+		Sk1: info.CertAs,
+		Sk2: info.CertBs,
+		Sk3: info.CertCs,
+		Sk4: info.CertDs,
 	}
 
-	if err := Db.Create(&newHeart).Error; err != nil {
+	if err := Db.Create(&newCert).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err})
 		return
 	}
 
 	if err := record.Updates(models.User{
-		S2Data: info.S2Data,
+		Certgiven: true,
 	}).Error; err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Something went wrong, Please try again."})
 		return
 	}
+
+	c.JSON(http.StatusAccepted, gin.H{"message": "Certificates Sent Successfully !!"})
 }
 
-func getActiveUsers(c *gin.Context, condition string, columnName string) {
-	// var users []models.User
-	var res []models.UserPublicKey
-	fetchUsers := Db.Model(&models.User{}).Where(condition).Pluck(columnName, &res)
-	if fetchUsers.Error != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Some Error Occurred"})
-		return
-	}
-	c.JSON(http.StatusOK, gin.H{"users": res})
-}
+// TODO: Do not delete this function
+// func getActiveUsers(c *gin.Context, condition string, columnName string) {
+// 	// var users []models.User
+// 	var res []models.UserPublicKey
+// 	fetchUsers := Db.Model(&models.User{}).Where(condition).Pluck(columnName, &res)
+// 	if fetchUsers.Error != nil {
+// 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Some Error Occurred"})
+// 		return
+// 	}
+// 	c.JSON(http.StatusOK, gin.H{"users": res})
+// }
 
 func GetStage1ActiveUsers(c *gin.Context) {
-	// getActiveUsers(c, "dirty = true", "id")
+	// getActiveUsers(c, "dirty = true", "id")  // TODO: See if a common function serves the purpose & with efficiency.
 	var users []models.User
 	var userDB models.User
 	Db.Model(userDB).Find(&users)
@@ -305,11 +554,31 @@ func GetStage1ActiveUsers(c *gin.Context) {
 }
 
 func GetStage2ActiveUsers(c *gin.Context) {
-	getActiveUsers(c, "s1submit = true", "id")
+	// getActiveUsers(c, "s1submit = true", "id") // TODO: See if a common function serves the purpose & with efficiency.
+	var users []models.User
+	var userDB models.User
+	Db.Model(userDB).Find(&users)
+	var results []string
+	for _, user := range users {
+		if user.S1submit {
+			results = append(results, user.Id)
+		}
+	}
+	c.JSON(http.StatusOK, gin.H{"users": results})
 }
 
 func GetStage3ActiveUsers(c *gin.Context) {
-	getActiveUsers(c, "s2submit = true", "id")
+	// getActiveUsers(c, "s2submit = true", "id") // TODO: See if a common function serves the purpose & with efficiency.
+	var users []models.User
+	var userDB models.User
+	Db.Model(userDB).Find(&users)
+	var results []string
+	for _, user := range users {
+		if user.S2submit {
+			results = append(results, user.Id)
+		}
+	}
+	c.JSON(http.StatusOK, gin.H{"users": results})
 }
 
 func FetchPublicKeys(c *gin.Context) {
--- models/heart.go
+++ models/heart.go
@@ -7,44 +7,47 @@ import (
 type (
 	Stage1 struct {
 		gorm.Model
-		Id string `json:"id" bson:"id" gorm:"unique"`
-		M1 string `json:"m1" bson:"m1" gorm:"unique"`
-		M2 string `json:"m2" bson:"m2" gorm:"unique"`
-		M3 string `json:"m3" bson:"m3" gorm:"unique"`
-		M4 string `json:"m4" bson:"m4" gorm:"unique"`
+		Id     string `json:"id" bson:"id" gorm:"unique"`
+		M1     string `json:"m1" bson:"m1" gorm:"unique"`
+		M2     string `json:"m2" bson:"m2" gorm:"unique"`
+		M3     string `json:"m3" bson:"m3" gorm:"unique"`
+		M4     string `json:"m4" bson:"m4" gorm:"unique"`
+		Submit bool   `json:"submit" bson:"submit"`
 	}
 )
 
 type (
 	Stage2 struct {
 		gorm.Model
-		Id    string `json:"id" bson:"id" gorm:"unique"`
-		Ka1   string `json:"ka1" bson:"ka1"`
-		Ka2   string `json:"ka2" bson:"ka2"`
-		Ka3   string `json:"ka3" bson:"ka3"`
-		Ka4   string `json:"ka4" bson:"ka4"`
-		Kb1   string `json:"kb1" bson:"kb1"`
-		Kb2   string `json:"kb2" bson:"kb2"`
-		Kb3   string `json:"kb3" bson:"kb3"`
-		Kb4   string `json:"kb4" bson:"kb4"`
-		Kc1   string `json:"kc1" bson:"kc1"`
-		Kc2   string `json:"kc2" bson:"kc2"`
-		Kc3   string `json:"kc3" bson:"kc3"`
-		Kc4   string `json:"kc4" bson:"kc4"`
-		Kd1   string `json:"kd1" bson:"kd1"`
-		Kd2   string `json:"kd2" bson:"kd2"`
-		Kd3   string `json:"kd3" bson:"kd3"`
-		Kd4   string `json:"kd4" bson:"kd4"`
-		CertA string `json:"certA" bson:"certA"`
-		CertB string `json:"certB" bson:"certB"`
-		CertC string `json:"certC" bson:"certC"`
-		CertD string `json:"certD" bson:"certD"`
+		Id     string `json:"id" bson:"id" gorm:"unique"`
+		Ka1    string `json:"ka1" bson:"ka1"`
+		Ka2    string `json:"ka2" bson:"ka2"`
+		Ka3    string `json:"ka3" bson:"ka3"`
+		Ka4    string `json:"ka4" bson:"ka4"`
+		Kb1    string `json:"kb1" bson:"kb1"`
+		Kb2    string `json:"kb2" bson:"kb2"`
+		Kb3    string `json:"kb3" bson:"kb3"`
+		Kb4    string `json:"kb4" bson:"kb4"`
+		Kc1    string `json:"kc1" bson:"kc1"`
+		Kc2    string `json:"kc2" bson:"kc2"`
+		Kc3    string `json:"kc3" bson:"kc3"`
+		Kc4    string `json:"kc4" bson:"kc4"`
+		Kd1    string `json:"kd1" bson:"kd1"`
+		Kd2    string `json:"kd2" bson:"kd2"`
+		Kd3    string `json:"kd3" bson:"kd3"`
+		Kd4    string `json:"kd4" bson:"kd4"`
+		CertA  string `json:"certA" bson:"certA"`
+		CertB  string `json:"certB" bson:"certB"`
+		CertC  string `json:"certC" bson:"certC"`
+		CertD  string `json:"certD" bson:"certD"`
+		Submit bool   `json:"submit" bson:"submit"`
 	}
 )
 
 type (
 	Stage3 struct {
 		gorm.Model
+		Id  string `json:"id" bson:"id" gorm:"unique"`
 		Sk1 string `json:"sk1" bson:"sk1"`
 		Sk2 string `json:"sk2" bson:"sk2"`
 		Sk3 string `json:"sk3" bson:"sk3"`
@@ -53,33 +56,40 @@ type (
 )
 
 type Stage1_Hearts struct {
-	M1 string `json:"m1" bson:"m1" gorm:"unique"`
-	M2 string `json:"m2" bson:"m2" gorm:"unique"`
-	M3 string `json:"m3" bson:"m3" gorm:"unique"`
-	M4 string `json:"m4" bson:"m4" gorm:"unique"`
+	M1     string `json:"m1" bson:"m1" gorm:"unique"`
+	M2     string `json:"m2" bson:"m2" gorm:"unique"`
+	M3     string `json:"m3" bson:"m3" gorm:"unique"`
+	M4     string `json:"m4" bson:"m4" gorm:"unique"`
 	S1Data string `json:"S1Data" bson:"S1Data" gorm:"unique"`
 }
 
 type Stage2_Hearts struct {
-	Ka1 string `json:"Ka1" bson:"Ka1" gorm:"unique"`
-	Ka2 string `json:"Ka2" bson:"Ka2" gorm:"unique"`
-	Ka3 string `json:"Ka3" bson:"Ka3" gorm:"unique"`
-	Ka4 string `json:"Ka4" bson:"Ka4" gorm:"unique"`
-	Kb1 string `json:"Kb1" bson:"Kb1" gorm:"unique"`
-	Kb2 string `json:"Kb2" bson:"Kb2" gorm:"unique"`
-	Kb3 string `json:"Kb3" bson:"Kb3" gorm:"unique"`
-	Kb4 string `json:"Kb4" bson:"Kb4" gorm:"unique"`
-	Kc1 string `json:"Kc1" bson:"Kc1" gorm:"unique"`
-	Kc2 string `json:"Kc2" bson:"Kc2" gorm:"unique"`
-	Kc3 string `json:"Kc3" bson:"Kc3" gorm:"unique"`
-	Kc4 string `json:"Kc4" bson:"Kc4" gorm:"unique"`
-	Kd1 string `json:"Kd1" bson:"Kd1" gorm:"unique"`
-	Kd2 string `json:"Kd2" bson:"Kd2" gorm:"unique"`
-	Kd3 string `json:"Kd3" bson:"Kd3" gorm:"unique"`
-	Kd4 string `json:"Kd4" bson:"Kd4" gorm:"unique"`
-	CertA string `json:"CertA" bson:"CertA" gorm:"unique"`
-	CertB string `json:"CertB" bson:"CertB" gorm:"unique"`
-	CertC string `json:"CertC" bson:"CertC" gorm:"unique"`
-	CertD string `json:"CertD" bson:"CertD" gorm:"unique"`
+	Ka1    string `json:"Ka1" bson:"Ka1" gorm:"unique"`
+	Ka2    string `json:"Ka2" bson:"Ka2" gorm:"unique"`
+	Ka3    string `json:"Ka3" bson:"Ka3" gorm:"unique"`
+	Ka4    string `json:"Ka4" bson:"Ka4" gorm:"unique"`
+	Kb1    string `json:"Kb1" bson:"Kb1" gorm:"unique"`
+	Kb2    string `json:"Kb2" bson:"Kb2" gorm:"unique"`
+	Kb3    string `json:"Kb3" bson:"Kb3" gorm:"unique"`
+	Kb4    string `json:"Kb4" bson:"Kb4" gorm:"unique"`
+	Kc1    string `json:"Kc1" bson:"Kc1" gorm:"unique"`
+	Kc2    string `json:"Kc2" bson:"Kc2" gorm:"unique"`
+	Kc3    string `json:"Kc3" bson:"Kc3" gorm:"unique"`
+	Kc4    string `json:"Kc4" bson:"Kc4" gorm:"unique"`
+	Kd1    string `json:"Kd1" bson:"Kd1" gorm:"unique"`
+	Kd2    string `json:"Kd2" bson:"Kd2" gorm:"unique"`
+	Kd3    string `json:"Kd3" bson:"Kd3" gorm:"unique"`
+	Kd4    string `json:"Kd4" bson:"Kd4" gorm:"unique"`
+	CertA  string `json:"CertA" bson:"CertA" gorm:"unique"`
+	CertB  string `json:"CertB" bson:"CertB" gorm:"unique"`
+	CertC  string `json:"CertC" bson:"CertC" gorm:"unique"`
+	CertD  string `json:"CertD" bson:"CertD" gorm:"unique"`
 	S2Data string `json:"S2Data" bson:"S2Data" gorm:"unique"`
+}
+
+type Share_Cert struct {
+	CertAs string `json:"CertAs" bson:"CertAs" gorm:"unique"`
+	CertBs string `json:"CertBs" bson:"CertBs" gorm:"unique"`
+	CertCs string `json:"CertCs" bson:"CertCs" gorm:"unique"`
+	CertDs string `json:"CertDs" bson:"CertDs" gorm:"unique"`
 }
